e05cbe98557e97c644c8f4e45e6971d420b322b2
Omar Rajjouleh <omarrojouleh@gmail.com>
2025-09-01 09:18:41 +0300

    Update Two_sum.py
    #Two sum problem url(https://leetcode.com/problems/two-sum/)

--- leetcode/Array/Two_sum.py
+++ leetcode/Array/Two_sum.py
@@ -1,4 +1,4 @@
-
+#Two sum problem url(https://leetcode.com/problems/two-sum/)
 class Solution(object):
     def twoSum(self, nums, target):
         """
@@ -17,8 +17,8 @@ class Solution(object):
 
         return []
 
-
-
-
+#complexity time=o(n) and space is o(n) we used extra space to decrease the time from o(n^2) bute force approach  to o(n) just by using dictionary 
+#the thing is just by looking in the keys of the dictionary it take o(1) it very rare but it can reach o(n) to be honest but it's o(1) in like 99% of use cases so its very rare to be o(n)
+#and by that its was solved in o(n) time 
 
 
