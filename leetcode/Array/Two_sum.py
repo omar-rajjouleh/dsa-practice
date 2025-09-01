0a03afdb65281896de1fd0e9db0e5f4d390007d6
Omar Rajjouleh <omarrojouleh@gmail.com>
2025-09-01 09:12:34 +0300

    Update Two_sum.py

--- leetcode/Array/Two_sum.py
+++ leetcode/Array/Two_sum.py
@@ -1,1 +1,24 @@
 
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        nums_dic = {}
+        for index, value in enumerate(nums):
+            nums_dic[value] = index
+
+        for index, value in enumerate(nums):
+            if target - value in nums_dic and index != nums_dic[target - value]:
+                return [index, nums_dic[target - value]]
+
+        return []
+
+
+
+
+
+
