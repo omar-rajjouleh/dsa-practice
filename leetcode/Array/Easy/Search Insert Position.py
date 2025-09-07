8deeb66b80592dcd3db162c96f66b9b982871396
Omar Rajjouleh <omarrojouleh@gmail.com>
2025-09-07 13:56:44 +0300

    Update Search Insert Position.py

--- leetcode/Array/Easy/Search Insert Position.py
+++ leetcode/Array/Easy/Search Insert Position.py
@@ -1,4 +1,5 @@
 #just binary search problem very easy the trick is to return the place where is will be the target if its of bound simple
+#url(https://leetcode.com/problems/search-insert-position/description/?envType=problem-list-v2&envId=array)
 class Solution(object):
     def searchInsert(self, nums, target):
         left, right = 0, len(nums) - 1
