05d0456a3099879a3c42c3b0bf89bd739723093f
Omar Rajjouleh <omarrojouleh@gmail.com>
2025-10-07 12:02:15 +0300

    Update Valid Anagram.py

--- leetcode/String/Easy/Valid Anagram.py
+++ leetcode/String/Easy/Valid Anagram.py
@@ -9,16 +9,12 @@ class Solution(object):
           return False
       s_dict={}
       t_dict={}
-      for char in s:
-          s_dict[char]=s_dict.get(char,0)+1
-      for char in t:
-          t_dict[char]=t_dict.get(char,0)+1
+      for i in range(len(s)):
+          s_dict[s[i]] = s_dict.get(s[i], 0) + 1
+          t_dict[t[i]] = t_dict.get(t[i], 0) + 1
 
-      for key in s_dict:
-          if key in t_dict:
-              if s_dict[key] != t_dict[key]:
-                  return False
-          else:
+      for char in s_dict:
+          if s_dict[char] != t_dict.get(char, 0):
               return False
-
       return True
+
